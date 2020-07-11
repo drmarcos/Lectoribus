56cd4121f14aeed89d8e64f6e5bbe8b8643f45b9
Marcos J.N. Oliveira <58213376+drmarcos@users.noreply.github.com>
2020-07-11 15:25:10 -0300

    Add files via upload

---
+++ temas/sampa/topo.php
@@ -0,0 +1,39 @@
+<?php
+/*-----------------------------------------------------------------------------------*
+@		SISTEMA DE GERENCIAMENTO DE CONTEÚDOS PARA SITES INTEGRADO AO MOODLE, 
+@		VENDA DE CURSOS E MATRÍCULAS AUTOMATIZADAS 
+@		{SOORDLE - SGCS - CMS}    	 
+@		Idioma: Português - Brasil	            						 
+@		Autor: 	Oliveira M.J.N
+@		Contato: <soordle@gmail.com>							                     								 	 
+@       © todos os direitos reservados desde 2007  
+@       VERSÃO 1.0     								 
+@
+@ NOTICE OF COPYRIGHT ---------------------------------------------------------------*                   
+@
+@ Copyright (C) 2007  Oliveira M.J.N  http://www.eadgames.com.br        
+@ Copyright (C) 2012  Oliveira M.J.N  http://www.soordle.org                    
+@
+@ This program is free software; you can redistribute it and/or modify  
+@ it under the terms of the GNU General Public License as published by  
+@ the Free Software Foundation; either version 2 of the License, or     
+@ (at your option) any later version.                                   
+@
+@ This program is distributed in the hope that it will be useful,       
+@ but WITHOUT ANY WARRANTY; without even the implied warranty of        
+@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
+@ GNU General Public License for more details:                          
+@
+@          http://www.gnu.org/copyleft/gpl.html                         
+@                                                                       
+@------------------------------------------------------------------------------------*/
+print"$META_TAGS<link rel=\"stylesheet\" type=\"text/css\" href=\"$ROTA/temas/$tema/css.css\">";
+$CFG->imagemLogomarca = $CFG->caminhoTemasImagens."xmlsgm.png";
+$CFG->imagemLogomarcaReduzida = $CFG->caminhoTemasImagens."xmlsgm.png";
+print "<div class=\"corpo\">";
+	$MJO->NOTB('class="topo" border="0" cellspacing="0"');
+	$MJO->NTR();
+		$o = new printConteudo($MJO->imagensTag($CFG->imagemLogomarca,"logo","","",""));
+	$MJO->NCTB();
+
+?>

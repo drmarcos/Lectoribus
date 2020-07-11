576d37cf38259e41adff503f9c25449dd9fd731f
Marcos J.N. Oliveira <58213376+drmarcos@users.noreply.github.com>
2020-07-11 15:13:42 -0300

    Add files via upload

---
+++ modulos/platao/idiomas/pt_br/textos.leitura.inc.php
@@ -0,0 +1,37 @@
+<?php
+$_TEXTO_PARA_LEITURA1 = "<b id=\"destaque11\">Platão</b><br/><br/>
+Platão  foi um dos principais filósofos gregos da Antiguidade. Ele nasceu em Atenas, por volta de 427/28, foi seguidor de Sócrates e mestre de Aristóteles. O nome pelo qual ficou conhecido era possivelmente um apelido, aparentemente ele se chamava Arístocles.
+
+<br/><br/>
+Este filósofo se encontrava no limiar de uma época, entre os valores antigos e um novo mundo que emergia, o que lhe propiciou uma riqueza de idéias sem igual. Ele tinha o poder de abordar os temas mais diversos, mais com a força da paixão e da criatividade artística do que com a lucidez da razão. Sua obra é um dos maiores legados da Humanidade, abrangendo debates sobre ética, política, metafísica e teoria do conhecimento.
+<br/><br/>
+Ao contrário de Sócrates, que vinha de uma origem humilde, Platão era integrante de uma família rica, de antiga e nobre linhagem. Ele conheceu seu ilustre mestre aos vinte anos. Sócrates era bem mais velho, pelo menos quarenta anos separavam ambos, mas eles puderam desfrutar de oito anos de aprendizado conjunto. Platão teve acesso também, por meio de seu professor, aos ideais pré-socráticos. Com a morte de seu preceptor, o filósofo isolou-se, com outros adeptos das idéias socráticas, em Mégara, ao lado de Euclides.
+<br/><br/>
+Depois de viajar pela Magna Grécia e pela Sicília, Platão regressou a Atenas e fundou a Academia, que em breve se tornou conhecida e freqüentada por um grande número de jovens que vinha à procura de uma educação melhor. Até intelectuais consagrados acorriam a esta instituição para debater suas idéias. Depois de várias tentativas de difundir seus conceitos políticos em Siracusa, na Sicília, Platão se instala definitivamente em sua terra natal, na liderança da Academia, até sua morte, em 347 a.C.
+<br/><br/>
+Dos filósofos da Antiguidade, Platão é o primeiro de quem se conhece a obra integral. Mas muitos de seus diálogos não são autênticos, embora supostamente assinados por ele. Seu estilo literário é o diálogo, uma espécie de ponte entre a oralidade fragmentária de Sócrates e a estética didática de Aristóteles. Nos escritos de Platão mesclam-se elementos mito-poéticos com fatores essencialmente racionais. Este filósofo não se guia pelo rigor científico, nem por uma metodologia formal.
+<br/><br/>
+Em Platão a filosofia ganha contornos e objetivos morais, apresentando assim soluções para os dilemas existenciais. Esta práxis, porém, assume no intelecto a forma especulativa, ou seja, para se atingir a meta principal do pensamento filosófico, é preciso obter o aprendizado científico. O âmbito da filosofia, para Platão, se amplia, se estende a tudo que existe. Segundo o filósofo, o homem vivencia duas espécies de realidade – a inteligível e a sensível. A primeira se refere à vida concreta, duradoura, não submetida a mudanças. A outra está ligada ao universo das percepções, de tudo que toca os sentidos, um real que sofre mutações e que reproduz neste plano efêmero as realidades permanentes da esfera inteligível. Este conceito é concebido como Teoria das Idéias ou Teoria das Formas.
+<br/><br/>
+Segundo Platão, o espírito humano se encontra temporariamente aprisionado no corpo material, no que ele considera a ‘caverna’ onde o ser se isola da verdadeira realidade, vivendo nas sombras, à espera de um dia entrar em contato concreto com a luz externa. Assim, a matéria é adversária da alma, os sentidos se contrapõem à mente, a paixão se opõe à razão. Para ele, tudo nasce, se desenvolve e morre. O Homem deve, porém, transcender este estado, tornar-se livre do corpo e então ser capaz de admirar a esfera inteligível, seu objetivo maior. O ser é irresistivelmente atraído de volta para este universo original através do que Platão chama de amor nostálgico, o famoso eros platônico.
+<br/><br/>
+Platão desenvolveu conceitos os mais diversos, transitando da metafísica para a política, destas para a teoria do conhecimento, abrangendo as principais esferas dos interesses humanos. Sua obra é estudada hoje em profundidade, apresentado uma atualidade inimaginável, quando se tem em vista que ela foi produzida há milênios, antes da vinda de Cristo. Seu pensamento influencia ainda em nossos dias teorias políticas, psicológicas – como a junguiana -, filosóficas, espirituais, sociológicas, entre outros segmentos do conhecimento humano.
+<b id=destaque3>Texto retirado de:<br/>
+http://www.infoescola.com/filosofia/platao/
+</b>
+<br/><br/>
+";
+
+$_OQUE_FAZER1 = "<b id=destaqueVerde>O que fazer?</b><br/><br/>Leia atentamente o texto abaixo, reflita sobre as indagações do autor e responda, com suas palavras, o questionário em seguida. ";
+$_DICA_FAZER1 = "<b id=destaqueVerde>Dica 1</b><br/><br/>Quando percebemos a intenção clara de ... ";
+$_DICA_FAZER2 = "<b id=destaqueVerde>Dica 2</b><br/><br/>Quando <b>não</b> percebemos a intenção clara de ... ";
+$_DICA_FAZER3 = "<b id=destaqueVerde>Dica 3</b><br/><br/>Não tenha pressa, você tem tempo suficiente para executar essa tarefa. ";
+
+$_PERGUNTA_UM = "O que você entende por interpretação?";
+$_PERGUNTA_DOIS = "Ler em voz alta faz diferença?";
+$_PERGUNTA_TRES = "Em poucas palavras, defina ambiguidade";
+$_PERGUNTA_QUATRO = "O que você entende por compreensão?";
+$_PERGUNTA_CINCO = "Analisando as últimas linhas ...?";
+$_PERGUNTA_SEIS = "Como você vê a relação entre...?";
+
+?>
